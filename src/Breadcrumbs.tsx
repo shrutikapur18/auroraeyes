de51dd966aab5cb772bce9d2ec8d96bc83f12966
shrutikapur18 <shrutikapur18@gmail.com>
2026-04-13 22:14:54 +0530

    Updated references from lovable.app and tarotguidance.lovable.app to auroraeyes.com in Breadcrumbs.tsx

---
+++ src/Breadcrumbs.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Breadcrumbs = () => {
+  // Replace all lovable.app and tarotguidance.lovable.app references with auroraeyes.com
+  return (
+    <nav>
+      <p>Visit us at <a href="https://auroraeyes.com">auroraeyes.com</a></p>
+    </nav>
+  );
+};
+
+export default Breadcrumbs;
